84343351e1a786944a5e9b700dc57de5705533fc
akshaygupta1998 <akshay.gupta998@gmail.com>
2020-11-12 17:29:11 +0530

    next

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Table from './Table';
 import Chair from './Chair';
 import Filter from './Filter';
 import SearchBar from './SearchBar';
-export { Chair, Filter, SearchBar, Table };
+export { Chair, Filter, Table, SearchBar };
+
